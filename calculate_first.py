f194e4a16449ad4405d7ca4735c5bdf835b4988e
Sonalkumar Kadelwar <sonalkumarkadelwar@gmail.com>
2018-04-09 02:21:57 +0530

    Add files via upload

---
+++ calculate_first.py
@@ -0,0 +1,50 @@
+class ff:
+  prod={}
+  terminals=[]
+  first={}
+  def isTerminal(self,c):
+    if c in self.terminals:
+      return True
+    else:
+      return False
+  def isNULL(self,c):
+    if c=='^':
+      return True
+    else:
+      return False
+  def isNonTerminal(self,c):
+    if c not in self.terminals:
+      if c!='^':
+        return True
+    else:
+      return False
+  def find_first(self,entry):
+    temp=self.prod[entry].split('|')
+    sdf=""
+    nt=""
+    for i in temp:
+      if self.isTerminal(i[0]):
+        sdf=sdf+i[0]
+      elif self.isNULL(i[0]):
+        sdf=sdf+i[0]
+      elif self.isNonTerminal(i[0]):
+        sdf=sdf+(self.find_first(i[0]))
+    print "sdf is "+sdf
+    return sdf;
+  def main(self):
+      n=int(input("Enter number of productions:"))
+      for i in range(0,n):
+        temp=raw_input("Enter production:")
+        temp=temp.split("->")
+        self.prod[temp[0]]=temp[1]
+      temp=raw_input("enter terminals with , separated:")
+      self.terminals=temp.split(",")
+      for i in self.prod:
+        self.first[i]=self.find_first(i)
+      print "first table is"
+      print(self.first)
+
+    
+    
+obj=ff()
+obj.main()
